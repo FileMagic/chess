c8967945abd9792591bd1428dc864f89897978cf
file_magic <file_magic@protonmail.com>
2023-02-26 15:42:28 -0500

    Update board to be usable

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,8 @@ struct Board {
   a1_h8_diagonal:       u64,
   h1_a8_anti_diagonal:  u64,
 }
-#[derive(PartialEq)]
+
+#[derive(PartialEq, Debug)]
 enum PieceType {
   Pawn,
   Knight,
@@ -28,6 +29,7 @@ enum PieceType {
   King
 }
 
+#[derive(Debug)]
 struct Move {
   from: Square,
   to: Square,
@@ -38,7 +40,7 @@ struct Move {
 
 // https://doc.rust-lang.org/book/ch06-01-defining-an-enum.html
 // https://locka99.gitbooks.io/a-guide-to-porting-c-to-rust/content/features_of_rust/enums.html
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Debug)]
 enum Square {
   A1=0,  B1=1,  C1=2,  D1=3,  E1=4,  F1=5,  G1=6,  H1=7,
   A2=8,  B2=9,  C2=10, D2=11, E2=12, F2=13, G2=14, H2=15,
@@ -58,8 +60,8 @@ impl Board {
     // This is using the Little-Endian Rank-File Mapping
     // This was useful while writing these: https://tearth.dev/bitboard-viewer/
     Board {
-      light_pieces:           0x55AA55AA55AA55AA,
-      dark_pieces:            0xAA55AA55AA55AA55,
+      light_pieces:           0x000000000000FFFF,
+      dark_pieces:            0xFFFF000000000000,
       pawns:                  0x00FF00000000FF00,
       bishops:                0x2400000000000024,
       knights:                0x4200000000000042,
@@ -131,7 +133,11 @@ impl Board {
   }
   
 }
-
+impl std::fmt::Display for Move{
+  fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+    write!(f, "from: {:#?}, to: {:#?}, piece_type: {:#?}, capture: {:#?}, promotion: {:#?},", self.from, self.to, self.piece_type, self.capture, self.promotion)
+  }
+}
 impl std::fmt::Display for Board {
   fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
       let mut s = String::new();
@@ -139,40 +145,42 @@ impl std::fmt::Display for Board {
           s.push_str(&format!("{} ", rank + 1));
           for file in 0..8 {
               let square = file + rank * 8;
-              let mut piece_str = " ";
+              let mut piece_str = String::from(" ");
               if self.light_pieces & (1 << square) != 0 {
                   if self.pawns & (1 << square) != 0 {
-                      piece_str = "P";
+                      piece_str = "♙".to_owned();
                   } else if self.knights & (1 << square) != 0 {
-                      piece_str = "N";
+                      piece_str = "♘".to_owned();
                   } else if self.bishops & (1 << square) != 0 {
-                      piece_str = "B";
+                      piece_str = "♗".to_owned();
                   } else if self.rooks & (1 << square) != 0 {
-                      piece_str = "R";
+                      piece_str = "♖".to_owned();
                   } else if self.queens & (1 << square) != 0 {
-                      piece_str = "Q";
+                      piece_str = "♕".to_owned();
                   } else if self.kings & (1 << square) != 0 {
-                      piece_str = "K";
+                      piece_str = "♔".to_owned();
+                      println!("light: {:#} {:#}, str: {}",file, rank, piece_str );
                   }
-                  piece_str = format!("{}+", piece_str).as_str();
+                  println!("{:#}",piece_str);
+                  s.push_str(format!("{}+", piece_str).as_str());
               } else if self.dark_pieces & (1 << square) != 0 {
                   if self.pawns & (1 << square) != 0 {
-                      piece_str = "p";
+                      piece_str = "♟︎".to_owned();
+                      println!("dark: {:#} {:#}",file, rank);
                   } else if self.knights & (1 << square) != 0 {
-                      piece_str = "n";
+                      piece_str = "♞".to_owned();
                   } else if self.bishops & (1 << square) != 0 {
-                      piece_str = "b";
+                      piece_str = "♝".to_owned();
                   } else if self.rooks & (1 << square) != 0 {
-                      piece_str = "r";
+                      piece_str = "♜".to_owned();
                   } else if self.queens & (1 << square) != 0 {
-                      piece_str = "q";
+                      piece_str = "♛".to_owned();
                   } else if self.kings & (1 << square) != 0 {
-                      piece_str = "k";
+                      piece_str = "♚".to_owned();
                   }
-                  piece_str = format!("{}-", piece_str).as_str();
+                  println!("{:#}",piece_str);
+                  s.push_str(format!("{}-", piece_str).as_str());
               }
-
-              s.push_str(piece_str);
           }
           s.push_str("\n");
       }
@@ -185,13 +193,15 @@ impl std::fmt::Display for Board {
 #[tokio::main]
 async fn main() -> color_eyre::Result<()> {
     color_eyre::install()?;
+    // fucking make the board
     let board = Board::default();
-     
-    let light_pieces = board.light_pieces.view_bits::<Lsb0>();
-    let dark_pieces = board.dark_pieces.view_bits::<Lsb0>();
     
-    // Print the board states for the eight different bit boards that I setup.
+    // Print the board state.
     println!("{}", board);
     
+    // Move b2 to b4.
+    let created_move = board.create_move(Square::B2, Square::B5);
+
+    println!("{:?}", created_move);
     Ok(())
 }
